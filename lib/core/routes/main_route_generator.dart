c605ce56fa2be1ff0a8d0c014580958cd40b5cf1
Elbarto <oyjddkvj@gmail.com>
2024-02-04 13:15:34 +0500

    route generator corrections

--- lib/core/routes/main_route_generator.dart
+++ lib/core/routes/main_route_generator.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:safar/core/routes/route_constants.dart';
 import 'package:safar/ui_staff/bildirgi_page/bildirgi_page.dart';
+import 'package:safar/ui_staff/signin_page/signin_page.dart';
 import 'package:safar/ui_staff/splash_page/splash_page.dart';
 import 'package:safar/ui_staff/staff_home_page/staff_home_page.dart';
 import 'package:safar/core/routes/cupertino_style_navigation_route.dart';
@@ -9,23 +10,20 @@ import 'package:safar/ui_staff/manage_inquiry_page/manage_inquiry_page.dart';
 class MainRouteGenerator {
   Route<dynamic>? generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      // case AppRoutes.splashPage:
-      //   return CustomCupertinoStyleNavigationRoute(
-      //     builder: (_) => const SplashPage(),
-      //   );
+      case AppRoutes.splashPage:
+        return CustomCupertinoStyleNavigationRoute(
+          builder: (_) => const SplashPage(),
+        );
 
-      // case AppRoutes.signIn:
-      //   return CustomCupertinoStyleNavigationRoute(
-      //     builder: (_) => const SignInPage(),
-      //   );
-      // case AppRoutes.homePage:
-      //   return CustomCupertinoStyleNavigationRoute(
-      //     builder: (_) => const HomePage(),
-      //   );
-      // case AppRoutes.pinPage:
-      //   return CustomCupertinoStyleNavigationRoute(
-      //     builder: (_) => const PinCodePage(),
-      //   );
+      case AppRoutes.signIn:
+        return CustomCupertinoStyleNavigationRoute(
+          builder: (_) => const SignInPage(),
+        );
+      case AppRoutes.homePage:
+        return CustomCupertinoStyleNavigationRoute(
+          builder: (_) => const StaffHomePage(),
+        );
+
       case AppRoutes.staffHome:
         return CustomCupertinoStyleNavigationRoute(
           builder: (_) => const StaffHomePage(),
