fd1b4afd41e6060240fac664e7c98ab3cc951a5b
Elbarto <oyjddkvj@gmail.com>
2024-03-08 23:20:00 +0500

    minor changes

--- lib/core/dialogs/sign_out_dialog.dart
+++ lib/core/dialogs/sign_out_dialog.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dialogs/flutter_dialogs.dart';
+import 'package:safar/core/api/api_provider.dart';
+import 'package:safar/core/box/current_user_box.dart';
 import 'package:safar/core/colors/app_colors.dart';
+import 'package:safar/core/routes/route_constants.dart';
+
+import '../db/preferences_services.dart';
 
 Future<dynamic> signOutDialog(BuildContext context) {
   return showPlatformDialog(
@@ -12,14 +17,14 @@ Future<dynamic> signOutDialog(BuildContext context) {
         BasicDialogAction(
           title: Text('Yes', style: _Style()),
           onPressed: () {
-            // PreferencesServices.clearAll().then((value) {
-            //   if (value) {
-            //     ApiProvider.create();
-            //     boxCurrentUser.clear();
+            PreferencesServices.clearAll().then((value) {
+              if (value) {
+                ApiProvider.create();
+                boxCurrentUser.clear();
 
-            //     Navigator.of(mycontext).pushNamed(AppRoutes.splashPage);
-            //   }
-            // });
+                Navigator.of(mycontext).pushNamed(AppRoutes.splashPage);
+              }
+            });
           },
         ),
         BasicDialogAction(
