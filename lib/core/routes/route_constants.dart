a4b2fc1eefa11e87a7024e1c61544f5f04f7b563
Elbarto <oyjddkvj@gmail.com>
2024-03-30 11:51:31 +0500

    ca

--- lib/core/routes/route_constants.dart
+++ lib/core/routes/route_constants.dart
@@ -1,28 +1,8 @@
 abstract class AppRoutes {
-  static const faceid = '/faceidpage';
-  static const pinPage = '/pinPage';
   static const splashPage = '/splashPage';
   static const signIn = '/signIn';
   static const homePage = '/homePage';
-  static const notificationsPage = '/notificationsPage';
-  static const materials = '/materialsPage';
-  static const qr = '/qrpage';
-  static const passcode = '/passcode';
   static const manageTaxiOrder = '/manageTaxiOrder';
   static const manageDeliveryOrder = '/manageDeliveryOrder';
   static const bildirgi = '/bildirgi';
-  static const profile = '/profile';
-  static const faceIDAndPasscodePage = '/faceidandpasscode';
-  static const attendacepage = '/attendancepage';
-  static const changePasscode = '/changePasscode';
-  static const createPasscode = '/createPasscode';
-  static const checkPasscode = '/checkPasscode';
-  static const editPage = '/editPage';
-  static const courses = '/courses';
-  static const questions = '/questions';
-  static const moduleQuestions = '/moduleQuestions';
-  static const moduleLessons = '/moduleLessons';
-  static const groupsStudentList = '/groupsStudentList';
-  static const assessmentResultPage = '/assessmentResultPage';
-  static const assessmentInfoEditPage = '/assessmentInfoEditPage';
 }
