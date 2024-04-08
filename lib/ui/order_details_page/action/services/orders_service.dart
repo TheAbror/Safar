0a758751067969609c8bfc57f0210eb5a986273c
Elbarto <oyjddkvj@gmail.com>
2024-04-08 13:26:13 +0500

    ca services

--- lib/ui/order_details_page/action/services/orders_service.dart
+++ lib/ui/order_details_page/action/services/orders_service.dart
@@ -9,21 +9,16 @@ abstract class OrdersService extends ChopperService {
   static OrdersService create([ChopperClient? client]) =>
       _$OrdersService(client ?? ChopperClient());
 
-  @Get(path: AppStrings.ordersList)
-  Future<Response<AllTaxiOrdersResponse>> getTaxiOrders();
-
-  @Get(path: AppStrings.deliveryList)
-  Future<Response<AllDeliveryOrdersResponse>> getDeliveryOrders();
-
   @Get(path: AppStrings.filterStatusesList)
   Future<Response<FilterStatusesList>> getStatusesList();
 
+  //Taxi
+
+  @Get(path: AppStrings.ordersList)
+  Future<Response<AllTaxiOrdersResponse>> getTaxiOrders();
+
   @Post(path: AppStrings.orderDetails)
   Future<Response<AllTaxiOrdersResponse>> postTaxiOrders(@Body() TaxiOrdersRequest body);
-
-  @Put(path: '${AppStrings.orderDetails}{id}/')
-  Future<Response<AllTaxiOrdersResponse>> editTaxiOrdersByID(
-      @Body() TaxiOrdersRequest body, @Path('id') int id);
 
   @Get(path: '${AppStrings.orderDetails}{id}/')
   Future<Response<TaxiOrdersResponse>> getOrderById(@Path('id') int id);
@@ -34,13 +29,17 @@ abstract class OrdersService extends ChopperService {
   @Delete(path: '${AppStrings.orderDetails}{id}/')
   Future<Response<DeleteOrderById>> deleteOrderById(@Path('id') int id);
 
+  @Put(path: '${AppStrings.orderDetails}{id}/')
+  Future<Response<AllTaxiOrdersResponse>> editTaxiOrdersByID(
+      @Body() TaxiOrdersRequest body, @Path('id') int id);
+
   // Delivery
+
+  @Get(path: AppStrings.deliveryList)
+  Future<Response<AllDeliveryOrdersResponse>> getDeliveryOrders();
 
   @Get(path: '${AppStrings.deliveryOrderDetails}{id}/')
   Future<Response<DeliveryOrdersResponse>> getDeliveryOrderById(@Path('id') int id);
-
-  @Delete(path: '${AppStrings.deliveryOrderDetails}{id}/')
-  Future<Response<DeleteOrderById>> deleteDeliveryOrderById(@Path('id') int id);
 
   @Post(path: AppStrings.deliveryOrderDetails)
   Future<Response<AllDeliveryOrdersResponse>> postDeliveryOrders(
@@ -48,6 +47,9 @@ abstract class OrdersService extends ChopperService {
 
   @Get(path: '${AppStrings.deliveryOrderDetails}{id}/')
   Future<Response<DeliveryOrdersResponse>> getDeliveryOrderByIdForEdit(@Path('id') int id);
+
+  @Delete(path: '${AppStrings.deliveryOrderDetails}{id}/')
+  Future<Response<DeleteOrderById>> deleteDeliveryOrderById(@Path('id') int id);
 
   @Put(path: '${AppStrings.deliveryOrderDetails}{id}/')
   Future<Response<AllDeliveryOrdersResponse>> editDeliveryOrdersByID(
