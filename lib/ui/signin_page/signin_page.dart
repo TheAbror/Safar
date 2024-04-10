d3493abd9b438f14c266c4083d8333574d938d58
Elbarto <oyjddkvj@gmail.com>
2024-04-10 15:57:50 +0500

    added stack to signin page

--- lib/ui/signin_page/signin_page.dart
+++ lib/ui/signin_page/signin_page.dart
@@ -28,136 +28,116 @@ class _SignInPageState extends State<SignInPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.float,
-      resizeToAvoidBottomInset: false,
-      body: BlocConsumer<AuthBloc, AuthState>(
-        listener: (context, state) async {
-          if (state.blocProgress == BlocProgress.IS_SUCCESS) {
-            Navigator.pushNamed(context, AppRoutes.homePage);
-          }
-          if (state.blocProgress == BlocProgress.IS_LOADING) {
-            Center(
-              child: CircularProgressIndicator(color: AppColors.primary),
-            );
-          } else if (state.blocProgress == BlocProgress.FAILED) {
-            showMessage(
-              state.failureMessage,
-              isError: true,
-            );
-          }
-        },
-        builder: (context, state) {
-          return Form(
-            key: _formKey,
-            child: Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.h),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 40.h),
-                  Center(
-                    child: Assets.icons.logoGreen.image(
-                      width: double.infinity,
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  Text(
-                    'Войти',
-                    style: TextStyle(
-                      fontSize: 24.sp,
-                      fontWeight: FontWeight.w500,
-                      color: Theme.of(context).colorScheme.tertiaryContainer,
-                    ),
-                  ),
-                  SizedBox(height: 2.h),
-                  Text(
-                    'Введите свои учетные данные для доступа к платформе',
-                    style: TextStyle(
-                      fontSize: 15.sp,
-                      fontWeight: FontWeight.w400,
-                      color: Theme.of(context).colorScheme.tertiaryContainer,
-                    ),
-                  ),
-                  SizedBox(height: 10.h),
-                  Text(
-                    'Логин',
-                    style: TextStyle(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.logInInTextColor,
-                    ),
-                  ),
-                  SizedBox(height: 5.h),
-                  SignInUsernameField(usernameController: _usernameController),
-                  SizedBox(height: 10.h),
-                  Text(
-                    'Номер',
-                    style: TextStyle(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.logInInTextColor,
-                    ),
-                  ),
-                  SizedBox(height: 5.h),
-                  ContactInfoField(contactsController: _contactsController),
-                  SizedBox(height: 10.h),
-                  Text(
-                    'Пароль',
-                    style: TextStyle(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w500,
-                      color: AppColors.logInInTextColor,
-                    ),
-                  ),
-                  SizedBox(height: 5.h),
-                  SignInPasswordField(passwordController: _passwordController),
-                  SizedBox(height: 5.h),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      GestureDetector(
-                        onTap: () => termsAndConditions(),
-                        child: Text(
-                          'Согласен с условиями использования',
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        backgroundColor: AppColors.float,
+        resizeToAvoidBottomInset: false,
+        body: BlocConsumer<AuthBloc, AuthState>(
+          listener: (context, state) async {
+            if (state.blocProgress == BlocProgress.IS_SUCCESS) {
+              Navigator.pushNamed(context, AppRoutes.homePage);
+            }
+            if (state.blocProgress == BlocProgress.IS_LOADING) {
+              Center(
+                child: CircularProgressIndicator(color: AppColors.primary),
+              );
+            } else if (state.blocProgress == BlocProgress.FAILED) {
+              showMessage(
+                state.failureMessage,
+                isError: true,
+              );
+            }
+          },
+          builder: (context, state) {
+            return Stack(
+              children: [
+                SingleChildScrollView(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.h),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        SizedBox(height: 40.h),
+                        Center(
+                          child: Assets.icons.logoGreen.image(
+                            width: double.infinity,
+                            fit: BoxFit.fill,
+                          ),
+                        ),
+                        Text(
+                          'Войти',
+                          style: TextStyle(
+                            fontSize: 24.sp,
+                            fontWeight: FontWeight.w500,
+                            color: Theme.of(context).colorScheme.tertiaryContainer,
+                          ),
+                        ),
+                        SizedBox(height: 2.h),
+                        Text(
+                          'Введите свои учетные данные для доступа к платформе',
+                          style: TextStyle(
+                            fontSize: 15.sp,
+                            fontWeight: FontWeight.w400,
+                            color: Theme.of(context).colorScheme.tertiaryContainer,
+                          ),
+                        ),
+                        SizedBox(height: 10.h),
+                        Text(
+                          'Логин',
                           style: TextStyle(
                             fontSize: 14.sp,
                             fontWeight: FontWeight.w500,
-                            color: AppColors.primary,
+                            color: AppColors.logInInTextColor,
                           ),
                         ),
-                      ),
-                      Checkbox(
-                        activeColor: AppColors.primary,
-                        value: state.isAgreedToTerms,
-                        onChanged: (value) {
-                          context.read<AuthBloc>().isAgreedToTerms(value ?? false);
-                        },
-                      ),
-                    ],
+                        SizedBox(height: 5.h),
+                        SignInUsernameField(usernameController: _usernameController),
+                        SizedBox(height: 10.h),
+                        Text(
+                          'Номер',
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.logInInTextColor,
+                          ),
+                        ),
+                        SizedBox(height: 5.h),
+                        ContactInfoField(contactsController: _contactsController),
+                        SizedBox(height: 10.h),
+                        Text(
+                          'Пароль',
+                          style: TextStyle(
+                            fontSize: 14.sp,
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.logInInTextColor,
+                          ),
+                        ),
+                        SizedBox(height: 5.h),
+                        SignInPasswordField(passwordController: _passwordController),
+                        SizedBox(height: 5.h),
+                        _AgreeWithTermsText(state, context),
+                        SizedBox(height: 250.h),
+                      ],
+                    ),
                   ),
-                  const Spacer(),
-                  _ContinueButton(
+                ),
+                Positioned(
+                  bottom: 30.h,
+                  right: 16.w,
+                  left: 16.w,
+                  child: _ContinueButton(
                     context,
                     state,
                     state.isAgreedToTerms,
                   ),
-                  SizedBox(height: 50.h),
-                ],
-              ),
-            ),
-          );
-        },
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
-  }
-
-  void termsAndConditions() async {
-    final result = await TermsBottomSheet.show(context);
-    if (result != null) {
-      if (!mounted) return;
-      context.read<AuthBloc>().isAgreedToTerms(result);
-    }
   }
 
   GestureDetector _ContinueButton(
@@ -172,13 +152,13 @@ class _SignInPageState extends State<SignInPage> {
               final username = _usernameController.text.trim();
               final password = _passwordController.text.trim();
               final contactInfo = _contactsController.text.trim();
+
               if (_formKey.currentState!.validate()) {
                 context.read<AuthBloc>().signIn(username, password, contactInfo);
               }
             },
       child: Container(
-        height: 48.h,
-        width: double.infinity,
+        height: 50.h,
         decoration: BoxDecoration(
           color: isAgreed ? AppColors.primary : Colors.grey.shade500,
           borderRadius: BorderRadius.all(Radius.circular(40.r)),
@@ -206,5 +186,39 @@ class _SignInPageState extends State<SignInPage> {
         ),
       ),
     );
+  }
+
+  Row _AgreeWithTermsText(AuthState state, BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        GestureDetector(
+          onTap: () => termsAndConditions(),
+          child: Text(
+            'Согласен с условиями использования',
+            style: TextStyle(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w500,
+              color: AppColors.primary,
+            ),
+          ),
+        ),
+        Checkbox(
+          activeColor: AppColors.primary,
+          value: state.isAgreedToTerms,
+          onChanged: (value) {
+            context.read<AuthBloc>().isAgreedToTerms(value ?? false);
+          },
+        ),
+      ],
+    );
+  }
+
+  void termsAndConditions() async {
+    final result = await TermsBottomSheet.show(context);
+    if (result != null) {
+      if (!mounted) return;
+      context.read<AuthBloc>().isAgreedToTerms(result);
+    }
   }
 }
