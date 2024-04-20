efd063bc3db51c6d31e4be539fa2a02f91dc5eed
Elbarto <oyjddkvj@gmail.com>
2024-04-20 17:52:35 +0500

    ca main route

--- lib/core/routes/main_route_generator.dart
+++ lib/core/routes/main_route_generator.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:safar/core/routes/route_constants.dart';
-import 'package:safar/ui/manage_order_page/manage_taxi_orders_page.dart';
-import 'package:safar/ui/order_details_page/delivery_details_page.dart';
-import 'package:safar/ui/order_details_page/taxi_details_page.dart';
+import 'package:safar/ui/home_page/root_page.dart';
 import 'package:safar/ui/signin_page/signin_page.dart';
 import 'package:safar/ui/splash_page/splash_page.dart';
-import 'package:safar/ui/home_page/root_page.dart';
+import 'package:safar/core/routes/route_constants.dart';
+import 'package:safar/ui/order_details_page/taxi_details_page.dart';
+import 'package:safar/ui/manage_order_page/manage_taxi_orders_page.dart';
+import 'package:safar/ui/order_details_page/delivery_details_page.dart';
 import 'package:safar/core/routes/cupertino_style_navigation_route.dart';
 import 'package:safar/ui/manage_order_page/manage_delievry_orders_page.dart';
 
@@ -27,28 +27,31 @@ class MainRouteGenerator {
           builder: (_) => const HomePage(),
         );
 
-      case AppRoutes.manageTaxiOrder:
-        return CustomCupertinoStyleNavigationRoute(
-          builder: (_) => ManageTaxiOrdersPage(
-            viewModel: settings.arguments as ManageTaxiOrdersPageViewModel,
-          ),
-        );
       case AppRoutes.taxiDetails:
         return CustomCupertinoStyleNavigationRoute(
           builder: (context) => TaxiDetailsPage(
             viewModel: settings.arguments as TaxiDetailsPageViewModel,
           ),
         );
-      case AppRoutes.manageDeliveryOrder:
+
+      case AppRoutes.manageTaxiOrder:
         return CustomCupertinoStyleNavigationRoute(
-          builder: (_) => ManagDeliveryOrdersPage(
-            viewModel: settings.arguments as ManagDeliveryOrdersPageViewModel,
+          builder: (_) => ManageTaxiOrdersPage(
+            viewModel: settings.arguments as ManageTaxiOrdersPageViewModel,
           ),
         );
+
       case AppRoutes.deliveryDetails:
         return CustomCupertinoStyleNavigationRoute(
           builder: (context) => DeliveryDetailsPage(
             viewModel: settings.arguments as DeliveryDetailsPageViewModel,
+          ),
+        );
+
+      case AppRoutes.manageDeliveryOrder:
+        return CustomCupertinoStyleNavigationRoute(
+          builder: (_) => ManagDeliveryOrdersPage(
+            viewModel: settings.arguments as ManagDeliveryOrdersPageViewModel,
           ),
         );
 
